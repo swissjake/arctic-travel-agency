6cc226661f10cf041121e7b27bfcde318bab93d7
Amgbara Sofiritari <38821635+swissjake@users.noreply.github.com>
2022-05-04 11:26:35 +0100

    Update BookTrip.tsx

--- src/components/BookTrip/BookTrip.tsx
+++ src/components/BookTrip/BookTrip.tsx
@@ -8,23 +8,23 @@ import checkin from '../../assets/checkin.svg'
 const BookTrip = () => {
   return (
       <section>
-        <div className='container p-2 md:p-0 md:relative'>
+            <div className='container p-2 md:p-0 md:relative'>
 
-            <div className='bookshadow md:w-[1200px] md:absolute bg-white rounded-[16px] md:z-40 md:h-[140px] md:mt-[-75px] md:left-[50%] md:translate-x-[-50%] md:space-x-[91px] md:flex-row flex flex-col p-7  items-center justify-center'>
-                <div className='mb-4 md:mb-0'>
-                    <div className='flex'>
-                        <img className="mr-[20px]" src={location} alt="/" />
-                         <p className='font-medium text-[#929191] text-[16px] tracking-[0.08em] leading-[24px] uppercase'>location</p>
+                <div className='bookshadow md:w-[1320px] md:absolute bg-white rounded-[16px] md:z-40 md:h-[140px] md:mt-[-75px] md:left-[50%] md:translate-x-[-50%] md:space-x-[120px] md:flex-row flex flex-col p-7  items-center justify-center'>
+                    <div className='mb-4 md:mb-0'>
+                        <div className='flex'>
+                            <img className="mr-[20px]" src={location} alt="/" />
+                            <p className='font-medium text-[#929191] text-[16px] tracking-[0.08em] leading-[24px] uppercase'>location</p>
+                        </div>
+                        <div className='flex'>
+                            <p className="booktrip">Iceland</p>
+                            <img className="ml-[8px]" src={dropdown} alt="/" />
+                        </div>
                     </div>
-                    <div className='flex'>
-                        <p className="booktrip">Iceland</p>
-                        <img className="ml-[8px]" src={dropdown} alt="/" />
-                    </div>
-                </div>
 
-                <div className='mb-4 md:mb-0'>
-                    <div className='flex'>
-                        <img className="mr-[20px]" src={user} alt="/" />
+                    <div className='mb-4 md:mb-0'>
+                        <div className='flex'>
+                            <img className="mr-[20px]" src={user} alt="/" />
                          <p className='font-medium text-[#929191] text-[16px] tracking-[0.08em] leading-[24px] uppercase'>Persons</p>
                     </div>
                     <div className='flex'>
@@ -58,6 +58,7 @@ const BookTrip = () => {
                 </div>
             </div>
     </section>
+    
     
   )
 }
